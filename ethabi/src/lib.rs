4151ca3d5cad04212d12df9ab875a56ff7aedfab
HackFisher <hackfisher@gmail.com>
2019-12-13 12:52:32 +0800

    Fix no string module

--- ethabi/src/lib.rs
+++ ethabi/src/lib.rs
@@ -1,4 +1,7 @@
 //! Ethereum ABI encoding decoding library.
+#![cfg_attr(not(feature = "std"), no_std)]
+// `construct_runtime!` does a lot of recursion and requires us to increase the limit to 256.
+#![recursion_limit="256"]
 
 #![warn(missing_docs)]
 
